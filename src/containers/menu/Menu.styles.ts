d927c3e4e7ae85951b2cdf4046b024c8891bd17c
k-lila <krishnalilapalandi@gmail.com>
2024-03-18 22:06:08 -0300

    Menu adjustments

--- src/containers/menu/Menu.styles.ts
+++ src/containers/menu/Menu.styles.ts
@@ -2,10 +2,11 @@ import styled from 'styled-components'
 
 export const MenuContainer = styled.div`
   position: fixed;
-  top: 95vh;
+  top: 90svh;
   left: 50%;
   transform: translate(-50%, -50%);
   display: flex;
   background-color: rgba(0, 0, 0, 0.5);
   padding: 0.5em;
+  z-index: 100;
 `
