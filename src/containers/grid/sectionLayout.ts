60f9f7a2297f420758f216cc69a41811e409bc90
k-lila <krishnalilapalandi@gmail.com>
2024-03-28 01:00:49 -0300

    cel skills projects layout

--- src/containers/grid/sectionLayout.ts
+++ src/containers/grid/sectionLayout.ts
@@ -10,8 +10,8 @@ type Layouts = {
 const sectionLayouts: Layouts = {
   about: {
     cel: {
-      columns: '5vw 20vw 11vw 10vw 5vw 5vw 5vw 24vw 5vw',
-      rows: '6svh 2svh 6svh 16svh 6svh 6svh 6svh 6svh 6svh 28svh 6svh'
+      columns: '5vw 40vw 2vw 3vw 4vw 2vw 5vw 24vw 5vw',
+      rows: '6svh 2svh 30svh 3svh 6svh 6svh 6svh 6svh 6svh 6svh'
     },
     tab: {
       columns: '10vw 5vw 2vw 4vw 4vw 6vw 8vw 13svw 15vw',
@@ -24,8 +24,8 @@ const sectionLayouts: Layouts = {
   },
   skills: {
     cel: {
-      columns: '5vw 25vw 10vw 4vw 5vw 10vw 8vw 18svw 5vw',
-      rows: '6svh 2svh 20svh 6svh 6svh 6svh 6svh 6svh 6svh 6svh 20svh'
+      columns: '5vw 5vw 10vw 20vw 20vw 12vw 8vw 5svw 5vw',
+      rows: '6svh 2svh 20svh 4svh 15svh 4svh 15svh 4svh 20svh 5svh'
     },
     tab: {
       columns: '10vw 10vw 32vw 10vw 10vw 10vw 10vw',
@@ -38,8 +38,8 @@ const sectionLayouts: Layouts = {
   },
   projects: {
     cel: {
-      columns: '10vw 10vw 10vw 10vw 10vw 10vw 10vw 10vw 10vw',
-      rows: '6svh 2svh 10svh 6svh 6svh 6svh 6svh 6svh 6svh 6svh 20svh'
+      columns: '5vw 5vw 5vw 15vw 30vw 15vw 3vw 7vw 5vw',
+      rows: '6svh 2svh 2svh 24svh 3svh 24svh 3svh 24svh 5svh 2svh'
     },
     tab: {
       columns: '10vw 10vw 10vw 10vw 10vw 10vw 32vw',
--- src/containers/grid/utils/boxGen.tsx
+++ src/containers/grid/utils/boxGen.tsx
@@ -1,11 +1,10 @@
+import React from 'react'
 import Box from '../../../components/box/box'
 import Menu from '../../menu/Menu'
 
 const setAbout = (boxes: Array<any>, device: string) => {
   if (device == 'cel') {
     boxes[2] = <Box key={2} $bgcolor="blue" />
-    // picture
-    boxes[7] = <Box key={7} $gcol="span 4" $grow="span 5" />
   } else {
     boxes[2] = <Box key={2} $bgcolor="red" />
   }
@@ -15,6 +14,15 @@ const setAbout = (boxes: Array<any>, device: string) => {
 const setSkills = (boxes: Array<any>, device: string) => {
   if (device == 'cel') {
     boxes[2] = <Box key={2} $bgcolor="violet" />
+    const modified = boxes.map((box, index) => {
+      if (index === 7 || index === 12 || index === 19 || index == 23) {
+        const newProps = { ...box.props, $bgcolor: 'wheat' }
+        return React.cloneElement(box, newProps)
+      } else {
+        return box
+      }
+    })
+    return modified
   } else {
     boxes[2] = <Box key={2} $bgcolor="orange" />
   }
@@ -24,6 +32,15 @@ const setSkills = (boxes: Array<any>, device: string) => {
 const setProjects = (boxes: Array<any>, device: string) => {
   if (device == 'cel') {
     boxes[2] = <Box key={2} $bgcolor="yellow" />
+    const modified = boxes.map((box, index) => {
+      if (index === 9 || index === 16 || index === 21) {
+        const newProps = { ...box.props, $bgcolor: 'wheat' }
+        return React.cloneElement(box, newProps)
+      } else {
+        return box
+      }
+    })
+    return modified
   } else {
     boxes[2] = <Box key={2} $bgcolor="green" />
   }
@@ -31,15 +48,28 @@ const setProjects = (boxes: Array<any>, device: string) => {
 }
 
 const boxGen = (section: string, device: string) => {
-  const boxes = Array.from({ length: 50 }, (_, index) => (
+  const boxes = Array.from({ length: 24 }, (_, index) => (
     <Box key={index}>{index}</Box>
   ))
-
-  boxes[3] = <Box key={3} $grow="span 4" />
-  boxes[4] = <Box key={4} $gcol="span 6" />
-  boxes[8] = <Box key={8} $gcol="span 4" />
-  boxes[11] = <Box key={11} $grow="span 4" />
-  boxes[12] = <Box key={12} $grow="span 8" />
+  boxes[4] = <Box key={4} $gcol="span 6"></Box>
+  boxes[5] = <Box key={5} $gcol="span 2" $grow="span 2"></Box>
+  boxes[6] = <Box key={6} $gcol="span 2"></Box>
+  boxes[7] = <Box key={7} $gcol="span 5"></Box>
+  boxes[8] = <Box key={8} $gcol="span 3"></Box>
+  boxes[9] = <Box key={9} $gcol="span 3"></Box>
+  boxes[10] = <Box key={10} $gcol="span 3"></Box>
+  boxes[11] = <Box key={11} $gcol="span 2"></Box>
+  boxes[12] = <Box key={12} $gcol="span 5"></Box>
+  boxes[13] = <Box key={13} $gcol="span 2"></Box>
+  boxes[14] = <Box key={14} $gcol="span 2" $grow="span 3"></Box>
+  boxes[16] = <Box key={16} $gcol="span 3"></Box>
+  boxes[17] = <Box key={17} $gcol="span 2"></Box>
+  boxes[19] = <Box key={19} $gcol="span 5"></Box>
+  boxes[20] = <Box key={20} $gcol="span 2" $grow="span 3"></Box>
+  boxes[21] = <Box key={21} $gcol="span 5"></Box>
+  boxes[22] = <Box key={22} $gcol="span 2" $grow="span 2"></Box>
+  boxes[23] = <Box key={23} $gcol="span 5"></Box>
+  boxes[24] = <Box key={24} $gcol="span 7"></Box>
 
   if (device == 'cel') {
     boxes[1] = (
