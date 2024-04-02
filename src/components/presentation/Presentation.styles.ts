30e446f358cfc4575c678cc310a97444cdfcc916
k-lila <krishnalilapalandi@gmail.com>
2024-04-01 23:04:09 -0300

    Presentation

--- src/components/presentation/Presentation.styles.ts
+++ src/components/presentation/Presentation.styles.ts
@@ -1,13 +1,18 @@
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
+
+const show = keyframes`
+  from { opacity: 0 }
+  to { opacity: 1 }
+`
 
 const PresentationStyled = styled.div`
   display: flex;
   flex-direction: column;
-  padding: 1%;
   height: 100%;
   width: 100%;
   justify-content: space-evenly;
   text-wrap: nowrap;
+  animation: ${show} 1s forwards;
   span {
     font-size: 1.5em;
     display: flex;
@@ -16,11 +21,11 @@ const PresentationStyled = styled.div`
   }
   > :nth-child(1) {
     font-weight: bold;
-    letter-spacing: 0.125em;
+    letter-spacing: 0.1em;
   }
 
   > :nth-child(3) {
-    margin-top: -4%;
+    margin-top: -2%;
     font-size: 1em;
     font-style: italic;
     img {
